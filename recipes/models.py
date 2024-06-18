ff97c80b833a77e4e6adfcfe4a7129e599980b8c
Kieran Chambers <kieran.chambers@icloud.com>
2024-06-18 19:25:13 -0400

    Feat: Create the Post model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_recipes")
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    total_cook_time = models.FloatField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
