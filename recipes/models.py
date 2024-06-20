44e22d896473da39a5bc9d639e33f973731b98de
Kieran Chambers <kieran.chambers@icloud.com>
2024-06-20 16:29:05 -0400

    Feat: Create Category model

--- recipes/models.py
+++ recipes/models.py
@@ -5,14 +5,34 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 # Create your models here.
+class Category(models.Model):
+    # category name limit of 50 characters & must be unique prevent duplicates
+    category_name = models.CharField(max_length=50, unique=True)
+    """
+    Foreign Key, one user can create many recipe categories
+    and SET_NULL ensures categories retained even if a user account is deleted
+    """
+    created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     slug = models.SlugField(max_length=150, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="user_recipes")
-    ingredients = models.TextField()
-    instructions = models.TextField()
+    recipe_category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="category_posts",
+        null=True)
+    ingredients = models.TextField(null=True)
+    instructions = models.TextField(null=True)
     total_cook_time = models.DurationField(
+        null=True,
+        blank=True,
+        default=None,
+        # help_text displays formatting instructions to the user
         help_text='Please enter the total cook time in the format HH:MM (e.g., 01:30 for 1 hour and 30 minutes).') # noqa
     created_on = models.DateTimeField(auto_now_add=True)
+    # default=0 sets default status of a new post to 'draft' not 'published'
     status = models.IntegerField(choices=STATUS, default=0)
