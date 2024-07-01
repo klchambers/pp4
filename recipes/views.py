c08f408a198035277854d239e514193471451891
Kieran Chambers <kieran.chambers@icloud.com>
2024-07-01 22:52:05 +0100

    Feat: Add back button to recipe page on index.html

--- recipes/views.py
+++ recipes/views.py
@@ -5,5 +5,5 @@ from .models import Recipe
 
 class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1)
-    template_name = "blog/index.html"
+    template_name = "recipes/index.html"
     paginate_by = 6
