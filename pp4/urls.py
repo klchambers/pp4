accf838a6211eeb55ea1c5fa2d966ea1a3d07495
Kieran Chambers <kieran.chambers@icloud.com>
2024-07-01 15:30:06 +0100

    Feat: Add generic recipe view to homepage

--- pp4/urls.py
+++ pp4/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from recipes.views import my_site
+from django.urls import path, include
 
 urlpatterns = [
+    path("", include("recipes.urls"), name="recipes-urls"),
     path('admin/', admin.site.urls),
-    path('recipes/', my_site, name='recipes'),
 ]
