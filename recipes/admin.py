d64db75d93615c49a6cd5c3e1e587821e31b8051
Kieran Chambers <kieran.chambers@icloud.com>
2024-07-01 13:24:39 +0100

    Feat: Register ModelAdmin classes for improved user experience in admin site

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
-from .models import Recipe, Category, Ingredient
+from .models import Recipe, Category, Ingredient, RecipeIngredient # noqa
 
 
-# Register your models here.
-admin.site.register(Recipe)
-admin.site.register(Category)
-admin.site.register(Ingredient)
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'created_by')
+    search_fields = ('category_name',)
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline,]
+    list_display = ('title', 'author', 'created_on', 'status')
+    search_fields = ('title', 'author__username')
+    list_filter = ('status', 'created_on', 'recipe_category')
+    # filter_horizontal = ('ingredients',)
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
